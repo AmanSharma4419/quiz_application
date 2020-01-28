3c504dc245a1c0031ac6ecc5a571a01908411c33
sharmaaman4419 <sharmaaman4419@gmail.com>
2020-01-28 12:37:00 +0530

    minor changes

--- client/components/CreateQuiz.jsx
+++ client/components/CreateQuiz.jsx
@@ -3,50 +3,70 @@ import React from "react";
 class CreateQuiz extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      question: [],
+      url: ""
+    };
   }
+
+  handleData = e => {
+    e.preventDefault();
+    if (questions.lenght === 0) {
+      var questions = [];
+    }
+    var x = this.state.question;
+    questions.push(x);
+    this.setState({ question: "" });
+  };
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
   render() {
     return (
       <React.Fragment>
-        <div
-          style={{
-            width: "300px",
-            display: "flex",
-            marginLeft: "39%",
-            marginTop: "10%"
-          }}
-        >
-          <h1 style={{ marginLeft: "35%" }}>AdminLogin</h1>
-          <hr />
-
-          <div className="field">
-            <p className="control has-icons-left has-icons-right">
-              <input
-                className="input"
-                type="email"
-                placeholder="Email"
-                name="email"
-                onChange={this.handleChange}
-              />
-              <span className="icon is-small is-left">
-                <i className="fas fa-envelope"></i>
-              </span>
-            </p>
-          </div>
-          <div className="field">
-            <p className="control has-icons-left">
-              <input
-                className="input"
-                type="password"
-                placeholder="Password"
-                name="password"
-                onChange={this.handleChange}
-              />
-              <span className="icon is-small is-left">
-                <i className="fas fa-lock"></i>
-              </span>
-            </p>
-          </div>
+        <div class="tile is-parent">
+          <article class="tile is-child notification is-info">
+            <p class="title">Create Quiz Questions</p>
+            <p class="subtitle">Add A Resource Url</p>
+            <center>
+              <form type="sub">
+                <div>
+                  <input
+                    style={{ width: "40%" }}
+                    className="input"
+                    type="text"
+                    placeholder="Enter Question"
+                    name="question"
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <br />
+                <button className="button is-link" onClick={this.handleData}>
+                  Add
+                </button>
+                <hr width="40%" />
+                <div>
+                  <input
+                    style={{ width: "40%" }}
+                    className="input"
+                    pattern="https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
+                    type="url"
+                    placeholder="Enter Url"
+                    name="url"
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <br />
+                <button className="button is-link" type="submit">
+                  Create
+                </button>
+              </form>
+            </center>
+            <figure class="image is-4by2"></figure>
+          </article>
         </div>
       </React.Fragment>
     );
